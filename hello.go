5d1d6d7ba47bd0c48eee50e2cdd552b8f245195d
Muhammad Haulul A <haululazkiya@gmail.com>
2022-10-02 14:14:19 +0700

    Create hello.go
    tugas 1 pengenalan pemrograman

---
+++ hello.go
@@ -0,0 +1,15 @@
+// Tugas membuat program hello.go berisi identitas diri (Minggu 1)
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Nama     : Muhammad Haulul Azkiyaa")
+	fmt.Println("NIM      : 1302223007")
+	fmt.Println("Kelas    : SE-46-02")
+	fmt.Println("Instansi : Telkom University")
+	fmt.Println("Email    : haululazkiyaa@student.telkomuniversity.ac.id")
+	fmt.Println("Linkedin : https://www.linkedin.com/in/haululazkiyaa/")
+	fmt.Println("GitHub   : https://github.com/haululazkiyaa")
+}
